cc4e934a2985d1ec3733afbfb2ac438f4a073450
Lucas Zombrilli <zombrilli.lucas@gmail.com>
2023-02-01 16:30:14 -0300

    cria listagem de tarefas e app imc

--- flutter_apps/appflutter/lib/model/tarefa.dart
+++ flutter_apps/appflutter/lib/model/tarefa.dart
@@ -7,23 +7,17 @@ class Tarefa {
 
   Tarefa(this._descricao, this._concluido);
 
-  String getId() {
-    return _id;
-  }
+  String get id => _id;
 
-  String getDescricao(descricao) {
-    return _descricao;
-  }
+  String get descricao => _descricao;
 
-  void setDescricao(String descricao) {
+  set descricao(String descricao) {
     _descricao = descricao;
   }
 
-  bool getConcluido(concluido) {
-    return _concluido;
-  }
+  bool get concluido => _concluido;
 
-  void setConcluido(bool concluido) {
+  set concluido(bool concluido) {
     _concluido = concluido;
   }
 }
---
+++ imcapp/android/app/src/main/kotlin/com/zombrilli/imc/imcapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zombrilli.imc.imcapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
